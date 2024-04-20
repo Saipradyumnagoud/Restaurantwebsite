f6d16efc632e904c54c4e8c6986e4ba828b736a2
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-04-20 14:46:49 +0530

    updated

--- save_booking.php
+++ save_booking.php
@@ -1,6 +1,6 @@
 <?php
 // Establish database connection
-$conn = new mysqli("localhost", "username", "password", "your_database");
+$conn = new mysqli("localhost", "saipradyumna", "sai", "restaurantdetails");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
